ddf9f1e3688b62c31c4fb81f690420defb1f63cf
Naren Sadhwani <naren.sadhwani@namisens.de>
2023-12-13 15:55:59 +0100

    Sweep Working, next to add stoppages

--- Threads/piMotorThread.py
+++ Threads/piMotorThread.py
@@ -3,16 +3,16 @@ from time import sleep
 
 import pandas as pd
 from PySide6.QtWidgets import QApplication
-from PySide6.QtCore import QThread
+from PySide6.QtCore import QThread, QMutex, QMutexLocker
 from PySide6.QtCore import QObject, Slot, Signal, QRunnable
 from pipython import GCSDevice
 from typing import Union
 from pipython.pitools import pitools
+from models.mainModel import calibrationUIModel
 
 
 class WorkerSignals(QObject):
     connection_status = Signal(int)
-    isThreadRunningSignal = Signal(bool)
     zAxisPosition = Signal(float)
     speedValue = Signal(float)
     minValue = Signal(float)
@@ -20,6 +20,8 @@ class WorkerSignals(QObject):
     nSweep = Signal(int)
     haltTime = Signal(float)
     stepSizeValue = Signal(float)
+    finished = Signal(bool)
+    m_finished = Signal(bool)
 
 
 class PiWorker(QObject):
@@ -29,15 +31,27 @@ class PiWorker(QObject):
     DEFAULT_ZAXIS_POSITION = 150
     STAGE_NUMBER = 'M-414.32S'
 
+
     def __init__(self, function=None, *args, **kwargs):
         super(PiWorker, self).__init__()
 
         self.serial_number = None
         self.piDevice: Union[GCSDevice, None] = None
+        self.stepSizeValue = None
+        self.zAxisPositionValue = None
+        self.speedValue = None
         self.args = args
         self.kwargs = kwargs
         self.function = function
+        self.nSweepValue = None
+        self.haltTimeValue = None
+        self.minValue = None
+        self.maxValue = None
+        self.cancelTimeValue = None
         self.deviceConnected = False
+        self.models = calibrationUIModel()
+        self.mutex = QMutex()
+        self.posReadingDict = {'N': [], 'MinPositionReading': [], 'MaxPositionReading': []}
 
         # Class Specific Flags
         self.isThreadRunning = False
@@ -45,10 +59,6 @@ class PiWorker(QObject):
 
         self.isThreadPaused: bool = False
         self.isThreadKilled: bool = False
-
-    @Slot(int)
-    def connectButtonStatus(self, state):
-        pass
 
     @Slot()
     def connectDevice(self):
@@ -59,7 +69,9 @@ class PiWorker(QObject):
             pitools.startup(self.piDevice, stages=[self.STAGE_NUMBER], refmodes=['FNL'])
             self.piDevice = self.piDevice
             self.deviceConnected = True
+            self.changeState(True)
             self.signals.connection_status.emit(1)
+
 
         except Exception as e:
             self.signals.connection_status.emit(0)
@@ -67,7 +79,6 @@ class PiWorker(QObject):
 
     def changeState(self, state):
         self.isThreadRunning = state
-        self.signals.isThreadRunningSignal.emit(self.isThreadRunning)
 
     def pause(self):
         self.isThreadPaused = not self.isThreadPaused
@@ -78,6 +89,7 @@ class PiWorker(QObject):
         self.piDevice = None
         self.signals.connection_status.emit(2)
         self.isThreadRunning = False
+        self.signals.finished.emit(True)
 
     def getPosition(self):
         return self.piDevice.qPOS('1')['1']
@@ -87,77 +99,118 @@ class PiWorker(QObject):
 
     @Slot()
     def run(self):
-        self.connectDevice()
+        # self.mutex.lock()
+        if self.piDevice is None:
+            self.connectDevice()
+
+        while True:
+            QApplication.processEvents()
+            time.sleep(0.2)
+            # print("Thread is running")
+        # self.mutex.unlock()
+
+################# Setters #################################################################################################################################
 
     @Slot(float)
     def set_z_axis_position(self, position):
-        self.signals.zAxisPosition = position
+        self.zAxisPositionValue = position
 
     @Slot(float)
     def set_speed(self, speed):
-        self.signals.speedValue = speed
+        self.speedValue = speed
 
     @Slot(float)
     def set_step_size(self, step_size):
-        self.signals.stepSizeValue = step_size
+        self.stepSizeValue = step_size
+
+    def set_min_sweep(self, min_value):
+        self.minValue = min_value
+
+    def set_max_sweep(self, max_value):
+        self.maxValue = max_value
+
+    def set_n_sweep(self, n_sweep):
+        self.nSweepValue = n_sweep
+
+    def set_halt_time(self, halt_time):
+        self.haltTimeValue = halt_time
+
+    def set_calibration_time(self, calibration_time):
+        self.calibrationTimeValue = calibration_time
+
+#########Move Functions###############################################################################################
 
     @Slot()
     def move(self):
         try:
 
-            self.piDevice.VEL('1', self.signals.speedValue)
-            self.piDevice.MOV('1', self.signals.zAxisPosition)
-            print("Moving at position {}, at speed {}".format(self.signals.zAxisPosition, self.signals.speedValue))
+            self.piDevice.VEL('1', self.speedValue)
+            self.piDevice.MOV('1', self.zAxisPositionValue)
+            print("Moving at position {} mm, at speed {} mm/sec".format(self.zAxisPositionValue, self.speedValue))
             if pitools.ontarget(self.piDevice, '1'):
                 self.signals.zAxisPosition.emit(self.getPosition())
-                print("Reached target position: {}".format(self.signals.zAxisPosition))
+                print("Reached target position: {}".format(self.zAxisPositionValue))
         except Exception as e:
+            self.signals.m_finished.emit(False)
             return e
+
+        finally:
+            self.signals.m_finished.emit(True)
 
     @Slot()
     def moveUp(self):
         try:
 
-            self.piDevice.VEL('1', self.signals.speedValue)
-            self.piDevice.MOV('1', self.signals.zAxisPosition)
-            print("Moving at position {}, at speed {}".format(self.signals.zAxisPosition, self.signals.speedValue))
-            print(f"{self.getPosition()} {self.signals.zAxisPosition}")
-            # self.signals.zAxisPosition.emit(self.getPosition())
+            self.piDevice.VEL('1', self.speedValue)
+            self.piDevice.MOV('1', self.zAxisPositionValue)
+            print("Moving at position {} mm, at speed {} mm/sec".format(self.zAxisPositionValue, self.speedValue))
+            if pitools.ontarget(self.piDevice, '1')and self.zAxisPositionValue == self.getPosition():
+                self.signals.zAxisPosition.emit(self.getPosition())
+                print("Reached target position: {} mm".format(self.zAxisPositionValue))
 
         except Exception as e:
             raise e
 
     @Slot()
     def moveDown(self):
-
-        self.piDevice.VEL('1', self.signals.speedValue)
-        self.piDevice.MOV('1', self.signals.zAxisPosition)
-        print("Moving at position {}, at speed {}".format(self.signals.zAxisPosition, self.signals.speedValue))
+        self.piDevice.VEL('1', self.speedValue)
+        self.piDevice.MOV('1', self.zAxisPositionValue)
+        print("Moving at position {} mm, at speed {} mm/sec".format(self.zAxisPositionValue, self.speedValue))
 
     @Slot()
     def pause(self):
-        # self.piDevice.VEL('1', 0)
         try:
             self.isThreadPaused = not self.isThreadPaused
-            self.piDevice.HLT('1')
-            self.piDevice.signals.zAixisPosition.emit(self.getPosition())
-        except Exception as GCSError:
-            raise GCSError
+            self.piDevice.STP('1')
+            self.signals.zAxisPosition.emit(self.getPosition())
+        except Exception as e:
+            raise e
 
+    @Slot()
+    def sweep(self):
+        self.piDevice.VEL('1', self.speedValue)
 
-# class Worker(QObject):
-#     super().__init__()
-#     self.signals = WorkerSignals()
-#     def connect(self):
-#         try:
-#             self.piDevice = GCSDevice(self.CONTROLLER_NUMBER)
-#             self.serial_number = self.piDevice.EnumerateUSB(self.CONTROLLER_NUMBER)[0]
-#             self.piDevice.ConnectUSB(self.serial_number)
-#             pitools.startup(self.piDevice, stages=[self.STAGE_NUMBER], refmodes=['FNL'])
-#             self.piDevice = self.piDevice
-#             self.deviceConnected = True
-#             self.signals.connection_status.emit(1)
-#
-#         except Exception as e:
-#             self.signals.connection_status.emit(0)
-#             raise e
+        for i in range(int(self.nSweepValue)):
+            self.posReadingDict['N'].append(i + 1)
+            # Start moving to the minimum position
+            self.piDevice.MOV('1', self.minValue)
+            pitools.waitontarget(self.piDevice, '1', postdelay=self.haltTimeValue)
+            sleep(self.calibrationTimeValue)
+            self.posReadingDict['MinPositionReading'].append(self.piDevice.qPOS('1')['1'])
+            sleep(self.calibrationTimeValue)
+            # Document minimum position readings
+
+            # Start moving to the maximum position
+            # PILogger.info("Moving to maximum position: {}".format(self.maxValue))
+            self.piDevice.MOV('1', self.maxValue)
+            pitools.waitontarget(self.piDevice, '1', postdelay=self.haltTimeValue)
+            # PILogger.info('Moved to max position: {}'.format(self.maxValue))
+
+            # Document maximum position readings
+            # PILogger.info("Getting maximum position readings")
+            sleep(self.calibrationTimeValue)
+            self.posReadingDict['MaxPositionReading'].append(self.piDevice.qPOS('1')['1'])
+            sleep(self.calibrationTimeValue)
+
+        self.df = pd.DataFrame.from_dict(self.posReadingDict)
+        self.df.to_csv("posReadings50.csv", index=False)
--- main.py
+++ main.py
@@ -16,38 +16,77 @@ class mainWindow(QMainWindow, Ui_calibrationUI):
     def __init__(self, parent=None):
         super(mainWindow, self).__init__(parent)
         self.threadPool = QThreadPool()
-        self.piDevice = None
-        self.model = calibrationUIModel()
+        self.connection_status = False
         self.ui = Ui_calibrationUI()
         self.ui.setupUi(self)
         self.worker = PiWorker()
-        self.deviceConnected = False
         self.thread = QThread()
+        self.is_already_connected = False
 
-        # Signals
+        # Connect Signals
         self.worker.signals.connection_status.connect(self.update_progress)
         self.ui.connectButton.clicked.connect(self._connect)
-        self.ui.Down.clicked.connect(self.down_clicked)
-        self.ui.Up.clicked.connect(self.up_clicked)
+
+        ######################################### Movement Tab #########################################
+
+        # Position Spin Box Signals
         self.ui.zAxisSpin.valueChanged.connect(self.worker.set_z_axis_position)
+        self.ui.stepSizeSpin.valueChanged.connect(self.worker.set_step_size)
+
+        # Speed Spin Box Signals
         self.ui.speedSpin.valueChanged.connect(self.worker.set_speed)
         self.worker.signals.stepSizeValue.connect(self.ui.stepSizeSpin.setValue)
-        self.ui.Move.clicked.connect(self.worker.move)
-        self.ui.Up.clicked.connect(self.worker.moveUp)
-        self.ui.Down.clicked.connect(self.worker.moveDown)
-        self.ui.pausePushButton_2.clicked.connect(self.worker.pause)
-        # self.worker.signals.zAxisPosition.connect(self.update_z_axis_position)
 
-        # self.show()
+        # Move Button Signals
+        self.ui.Move.clicked.connect(self.move_clicked)
+        self.ui.Move.clicked.connect(self.worker.move)
+
+        # Up Button Signals
+        self.ui.Up.clicked.connect(self.up_clicked)
+        self.ui.Up.clicked.connect(self.worker.moveUp)
+
+        # Down Button Signals
+        self.ui.Down.clicked.connect(self.worker.moveDown)
+        self.ui.Down.clicked.connect(self.down_clicked)
+
+        # Pause Button Signals
+        self.ui.pausePushButton_2.clicked.connect(self.pause_clicked)
+        self.ui.pausePushButton_2.clicked.connect(self.worker.pause)
+        self.worker.signals.zAxisPosition.connect(self.pause_clicked)
+
+        # Finished Movement Signal, called after every movement
+        self.worker.signals.m_finished.connect(self.update_movement)
+
+        ################## Calibration Tab########################################################
+
+        self.ui.sweepPushButton.clicked.connect(self.worker.sweep)
+
+        # Set Calibration Parameters
+        self.ui.nSweepSpin.valueChanged.connect(self.worker.set_n_sweep)
+        self.ui.measuringTImeSpin.valueChanged.connect(self.worker.set_halt_time)
+        self.ui.calibrationTimeSpin.valueChanged.connect(self.worker.set_calibration_time)
+        self.ui.maxSweepSpin.valueChanged.connect(self.worker.set_max_sweep)
+        self.ui.minSweepSpin.valueChanged.connect(self.worker.set_min_sweep)
+
+    def move_clicked(self):
+        self.ui.pausePushButton_2.setEnabled(True)
+
+    def update_movement(self, value):
+        self.ui.pausePushButton_2.setEnabled(False)
+        self.statusBar().showMessage("Moved to ".format(self.ui.zAxisSpin.value()))
+
+    def pause_clicked(self, value):
+        self.ui.zAxisSpin.setValue(value)
 
     @Slot()
     def up_clicked(self):
-
         self.ui.zAxisSpin.setValue(self.ui.zAxisSpin.value() - self.ui.stepSizeSpin.value())
+        self.statusBar().showMessage("Moving to {}".format(self.ui.zAxisSpin.value()))
 
     @Slot()
     def down_clicked(self):
         self.ui.zAxisSpin.setValue(self.ui.zAxisSpin.value() + self.ui.stepSizeSpin.value())
+        self.statusBar().showMessage("Moving to {}".format(self.ui.zAxisSpin.value()), 2000)
 
     @Slot(float)
     def update_z_axis_position(self, value):
@@ -62,17 +101,18 @@ class mainWindow(QMainWindow, Ui_calibrationUI):
             # print(self.piWorker.isRunning())
 
             # Update UI Connection Status
-
-            # print("Thread status from update_progress {}".format(self.workerThread.isRunning()))
-
-            # Enable Buttons and Line editing
-
             self.toggleButtons(True)
+            print("Thread status from update_progress {}".format(self.thread.isRunning()))
+
+            # Get Motor Position and Speed Values from the controller
             self.ui.zAxisSpin.setValue(self.worker.getPosition())
             self.ui.speedSpin.setValue(float(self.worker.getSpeed()))
 
         elif value == 2:
             self.ui.statusbar.showMessage("Disconnected", 5000)
+            self.toggleButtons(False)
+            self.ui.statusbar.showMessage(f"Disconnected")
+            self.ui.connectButton.setText('Connect')
 
         else:
             self.ui.statusbar.showMessage(f"Unable to find any controllers. "
@@ -80,27 +120,32 @@ class mainWindow(QMainWindow, Ui_calibrationUI):
 
     @Slot()
     def _connect(self):
-        if self.model.connectButtonState:
-
-            self.ui.statusbar.showMessage(f"Disconnected")
-            self.ui.connectButton.setText('Connect')
-            self.toggleButtons(False)
+        if self.connection_status:
             self.worker.kill()
+            print(self.thread.isFinished(), self.thread.isRunning())
 
         else:
             self.attach_worker()
             print(self.thread.isFinished(), self.thread.isRunning())
 
     def attach_worker(self):
-        self.worker.moveToThread(self.thread)
-        self.thread.started.connect(self.worker.run)
-        self.thread.start()
-        self.worker.signals.connection_status.connect(self.update_progress)
-        self.thread.finished.connect(self.thread.deleteLater)
+        if not self.is_already_connected:
+            self.worker.moveToThread(self.thread)
+            self.thread.started.connect(self.worker.run)
+            self.worker.signals.finished.connect(self.worker.deleteLater)
+            self.thread.finished.connect(self.thread.deleteLater)
+            self.worker.signals.finished.connect(self.thread.quit)
+            self.thread.start()
+            self.is_already_connected = True
+        else:
+
+            self.thread.start()
 
     def toggleButtons(self, state):
+
+        # Update Connection Flag
+        self.connection_status = state
         # Update UI Connection Status
-        self.model.connectButtonState = state
         self.ui.zAxisSpin.setEnabled(state)
         self.ui.speedSpin.setEnabled(state)
         self.ui.stepSizeSpin.setEnabled(state)
@@ -109,7 +154,6 @@ class mainWindow(QMainWindow, Ui_calibrationUI):
         self.ui.Up.setEnabled(state)
         self.ui.Down.setEnabled(state)
         self.ui.Move.setEnabled(state)
-        self.ui.pausePushButton_2.setEnabled(state)
         self.ui.nSweepSpin.setEnabled(state)
         self.ui.measuringTImeSpin.setEnabled(state)
         self.ui.calibrationTimeSpin.setEnabled(state)
